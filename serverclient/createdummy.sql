01c10041adde4bcf0d653c2ac45311b713e58d98
hawaecho <rugga.wang@gmail.com>
2015-03-16 21:15:00 +0800

    added a file named createdummy.sql for create a sql database

---
+++ serverclient/createdummy.sql
@@ -0,0 +1,21 @@
+-- cleanup old table
+DROP TABLE topictable CASCADE;
+
+-- create roles
+CREATE ROLE dbcreator WITH PASSWORD 'nifti';
+CREATE ROLE dbuser WITH PASSWORD 'nifti';
+ALTER ROLE dbuser WITH LOGIN;
+ALTER ROLE dbcreator WITH LOGIN;
+
+-- create the dummy table for test
+CREATE TABLE topictable (
+            "id" oid PRIMARY KEY,
+            "topic" text DEFAULT NULL,
+            "type" text DEFAULT NULL,
+            "timestamp" int8 DEFAULT NULL,
+            "target" oid DEFAULT NULL
+            )
+WITH (OIDS=FALSE);
+ALTER TABLE topictable OWNER TO "dbuser";
+GRANT ALL PRIVILEGES ON topictable TO dbcreator;
+GRANT SELECT ON topictable TO dbuser;
